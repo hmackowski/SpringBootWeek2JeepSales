567b65155d5ec4e2f9baa340843d89d613dde4d0
hmackowski <hmackowski@gmail.com>
2022-12-18 00:54:36 -0500

    first commit

---
+++ jeep-sales-homework/src/main/java/com/promineotech/jeep/service/DefaultJeepSalesService.java
@@ -0,0 +1,20 @@
+package com.promineotech.jeep.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class DefaultJeepSalesService implements JeepSalesService {
+  
+  @Override
+  public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+      log.info("The fetchJeeps method was called with model={} and trim={}", model, trim);
+      return null;
+  }
+
+}
